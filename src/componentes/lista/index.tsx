3d2b172e3d12b1f3e82c94ac675bdd90c0c09a93
Paulo <paulinhoguaruja@hotmail.com>
2023-05-24 20:51:21 -0300

    fazendo modificacoes no componente Lista

--- src/componentes/lista/index.tsx
+++ src/componentes/lista/index.tsx
@@ -1,19 +1,29 @@
 import React from "react"
 
 function Lista() {
+    const tarefas = [{
+        tarefa: "React",
+        tempo: "02:00:00"
+    },{
+        tarefa: "JavaScript",
+        tempo: "01:00:00"
+    },{
+        tarefa: "TypeScript",
+        tempo: "01:30:00"
+    }
+]
     return (
         <aside>
             <h2> Estudos do dia </h2>
             <ul>
-                <li>
-                    <h3>React</h3>
-                    <span>02:00:00</span>
-                </li>
-                <li>
-                    <h3>Javascript</h3>
-                    <span>01:00:00</span>
-                </li>
-            </ul>
+                {tarefas.map((item, index) => (
+                    <li key={index}>
+                        <h3> {item.tarefa} </h3>
+                        <span> {item.tempo}</span>
+                    </li>
+
+                ))}
+            </ul>   
         </aside>
     )
 }
