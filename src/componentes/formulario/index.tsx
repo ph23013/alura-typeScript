c3ff9d68f05608fbc08ceaf3572aa78291b2e992
Paulo <paulinhoguaruja@hotmail.com>
2023-05-24 21:51:53 -0300

    criando o stilo dos componetes em aula STYLE.SCCS

--- src/componentes/formulario/index.tsx
+++ src/componentes/formulario/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Botao from "../botao";
+import "./style.scss"
 
 class Formulario extends React.Component {
     render() {
         return(
-            <form>
-                <div>
+            <form className="novaTarefa">
+                <div className="inputContainer">
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                     </label>
                     <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required />
                 </div>
-                <div>
+                <div className="inputContainer">
                     <label htmlFor="tempo">
                         Tempo
                     </label>
