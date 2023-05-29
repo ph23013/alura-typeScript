b4a330dd0d0c187dd08d2e0e097c0b4ba34dd83d
Paulo <paulinhoguaruja@hotmail.com>
2023-05-29 19:21:52 -0300

    criando mais 2 componetes no projeto (cronometro/relogio)

---
+++ src/componentes/cronometro/index.tsx
@@ -0,0 +1,19 @@
+import Botao from "../botao"
+
+export default function Cronometro() {
+    return(
+        <div>
+            <p>Escolha um card e inicie o cronômetro</p>
+            <div>
+            
+            </div>        
+            <Botao>
+                Comecar!
+            </Botao>
+        
+        
+        </div>
+        )
+     
+    
+}
---
+++ src/componentes/cronometro/relogio/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Relogio() {
+    return(
+        <>
+        <span>0</span>
+        <span>0</span>
+        <span>:</span>
+        <span>0</span>
+        <span>0</span>
+        </>
+    )
+}
