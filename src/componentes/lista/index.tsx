409290b15e54eef065562595a6e9860c6ef7322b
Paulo <paulinhoguaruja@hotmail.com>
2023-05-29 18:51:10 -0300

    criando um componente de Item dentro da pasta lista

--- src/componentes/lista/index.tsx
+++ src/componentes/lista/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from'./lista.module.scss'
+import Item from './item'
 
 function Lista() {
     const tarefas = [{
@@ -18,10 +19,7 @@ function Lista() {
             <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3> {item.tarefa} </h3>
-                        <span> {item.tempo}</span>
-                    </li>
+                    <Item key={index} {...item} />
 
                 ))}
             </ul>   
