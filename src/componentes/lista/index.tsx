3b60ddc16d57f3b05d6cdcddae34688f907764a8
Paulo <paulinhoguaruja@hotmail.com>
2023-06-02 21:24:52 -0300

    testando state no projeto na lista (renderizacao no react)

--- src/componentes/lista/index.tsx
+++ src/componentes/lista/index.tsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from'./lista.module.scss'
 import Item from './item'
 
 function Lista() {
-    const tarefas = [{
-        tarefa: "React",
-        tempo: "02:00:00"
-    },{
-        tarefa: "JavaScript",
-        tempo: "01:00:00"
-    },{
-        tarefa: "TypeScript",
-        tempo: "01:30:00"
-    }
-]
+    const [tarefas, setTarefas] = useState([{
+        
+            tarefa: "React",
+            tempo: "02:00:00"
+        },{
+            tarefa: "JavaScript",
+            tempo: "01:00:00"
+        },{
+            tarefa: "TypeScript",
+            tempo: "01:30:00"
+        
+    }]);
     return (
         <aside className={style.listaTarefas}>
-            <h2> Estudos do dia </h2>
+            <h2 onClick={() => {setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}])}}> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item key={index} {...item} />
