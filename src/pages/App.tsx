a2d4e1cf50c9e64dac3f3b6326b27980c62a3af3
Paulo <paulinhoguaruja@hotmail.com>
2023-06-05 19:28:11 -0300

    apagando array do APP, UMA TESTE ATUALIZADO

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,20 +3,11 @@ import Formulario from '../componentes/formulario';
 import Lista from '../componentes/lista';
 import style from'./App.module.scss'
 import Cronometro from '../componentes/cronometro';
+import { iTarefa } from '../types/tarefa';
 
 function App() {
-  const [tarefas, setTarefas] = useState([{
+  const [tarefas, setTarefas] = useState<iTarefa[] | []>([]);
         
-    tarefa: "React",
-    tempo: "02:00:00"
-},{
-    tarefa: "JavaScript",
-    tempo: "01:00:00"
-},{
-    tarefa: "TypeScript",
-    tempo: "01:30:00"
-
-}]);
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas= {setTarefas}/>
